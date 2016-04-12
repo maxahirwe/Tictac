7f4f16f758caa583e080320955199f24ad743b91
tmaxletitgo <tmaxletitgo@gmail.com>
2016-04-12 12:27:02 +0800

    partially finished the game board interface design (the 9 boxes that comply a 3*3 Tic tac game.

---
+++ app/src/main/java/com/hillygeeks/tictac/SplashActivity.java
@@ -0,0 +1,22 @@
+package com.hillygeeks.tictac;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by max on 12/4/2016.
+ * <p/>
+ * represents the splash screen that will display a logo and launch the main game activity
+ */
+public class SplashActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        Intent intent = new Intent(this, MainActivity.class);
+        startActivity(intent);
+        finish();
+    }
+}
